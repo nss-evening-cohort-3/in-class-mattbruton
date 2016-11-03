8536b8a804c625f6fbc550d90f3a1b144c0317a4
MATT BRUTON <the.bruton@gmail.com>
2016-11-03 12:13:59 -0500

    step 9 completed

--- BasicsOfForms/Scripts/App/app.js
+++ BasicsOfForms/Scripts/App/app.js
@@ -1,1 +1,9 @@
 ﻿var app = angular.module("MyApp", []);
+
+app.controller("FormController", function ($scope, $http) {
+
+    $scope.formSubmit = function () {
+        console.log("yep, works");
+    };
+
+});
